4cd9d9068d364bfdc2605c6aeefb2f83fc63955a
Husanboy Azamov <azamovhusanboy08@gmail.com>
2025-01-15 18:45:25 +0500

    feat: Kategoriya uchun serializer qo'shildi

--- core/apps/portfolio/serializers/portfolio/Portfolio.py
+++ core/apps/portfolio/serializers/portfolio/Portfolio.py
@@ -1,16 +1,25 @@
 from rest_framework import serializers
 
-from ...models import PortfolioModel, SkilsModel
+from ...models import PortfolioModel, SkilsModel, CategoryModel
 from core.apps.portfolio.serializers.portfolio.Skils import BaseSkilsSerializer
 
+
+class CategorySerializers(serializers.ModelSerializer):
+    class Meta:
+        model = CategoryModel
+        fields = ['name']
+        
 
 class SkilsSerializers(serializers.ModelSerializer):
     class Meta:
         model = SkilsModel
         fields = ['name']
 
+
 class BasePortfolioSerializer(serializers.ModelSerializer):
     skils = SkilsSerializers(many=True)  
+    category = CategorySerializers()  
+    
 
     class Meta:
         model = PortfolioModel
